ffe3e4dc37ed7acaf8060c4e59ed0f63228396e6
margo <minmm98@gmail.com>
2023-08-16 14:52:14 +0800

    頁面更新

--- src/views/store/Datagrid.vue
+++ src/views/store/Datagrid.vue
@@ -30,10 +30,9 @@
         <div class="col-xl-2 col-lg-2 col-md-6 col-12">
           <p>設備分類</p>
           <div class="dropdown">
-            <button class="btn dropdown-toggle" type="button" id="categoryDropdown" data-bs-toggle="dropdown"
-              aria-haspopup="true" aria-expanded="false" :class="{ disabled: !(EquipTypeName !== '') }">
-              {{ EquipCategoryName || EquipCategoryInit }}
-            </button>
+            <button class="btn dropdown-toggle" type="button" id="categoryDropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" :class="{ disabled: !(EquipTypeName !== '') }">
+                {{ EquipCategoryName || EquipCategoryInit }}
+              </button>
             <div class="dropdown-menu" aria-labelledby="categoryDropdown">
               <p v-for="(item, index) in EquipCategoryArray" :key="index" class="dropdown-item" @click="selectCategory(`${item}`)">{{ item }}</p>
             </div>
@@ -47,7 +46,7 @@
           <p>物品名稱</p>
           <input type="text" v-model="AssetName" />
         </div>
-        <div class="col-xl-2 col-lg-2 col-md-6 col-12">
+        <div class="col-xl-3 col-lg-2 col-md-6 col-12">
           <p>狀態</p>
           <div class="dropdown">
             <button class="btn dropdown-toggle" type="button" id="statusDropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -83,8 +82,8 @@
               </div>
             </div>
           </div> -->
-        <div class="col-xl-2 col-lg-2 col-md-6 col-12">
-          <p>申請入庫日期（起</p>
+        <div class="col-xl-2 col-lg-2 col-md-6 col-12 flex-col">
+          <p>申請入庫日期(起)</p>
           <div class="date-selector">
             <div class="input-container">
               <input type="date" v-model="StartDate" class="date-input" />
@@ -113,183 +112,50 @@
 </template>
 
 <script>
-import { ref, onMounted } from "vue";
-import { AgGridVue } from "ag-grid-vue3";
-import Storage_return_button from "@/components/Storage_return_button";
-import Delete_button from "@/components/Delete_button";
-import Navbar from "@/components/Navbar.vue";
-import router from "@/router";
-
-export default {
-  components: {
-    Navbar,
-    AgGridVue,
-    Storage_return_button,
-    Delete_button,
-  },
-  setup() {
-    const EquipTypeName = ref(''); //設備總類 *必填
-    const EquipTypeArray = ref([]); //設備總類陣列 request拿到
-    const EquipCategoryName = ref(''); //設備分類 *必填
-    const EquipCategoryArray = ref([]); //設備分類陣列 request拿到
-    const EquipCategoryInit = ref('請先選擇設備總類');
-    // const AssetId = ref(''); //資產編號
-    const AssetName = ref(''); //物品名稱
-    const Status = ref(''); //狀態
-    const StatusArray = ref(['申請入庫', '申請歸還', '可交付', '待入庫', '已入庫', '已歸還',])
-    const StartDate = ref(''); //申請入庫日期(起)
-    const EndDate = ref(''); //申請入庫日期(迄)
-    const total = 100;
-    const pageSize = 10;
-    const pagePosition = ref("bottom");
-    const pageOptions = [
-      { value: "bottom", text: "Bottom" },
-      { value: "top", text: "Top" },
-      { value: "both", text: "Both" }
-    ];
-
-    const columnDefs = [
-      {
-        suppressMovable: true,
-        field: "",
-        cellRenderer: "Storage_return_button",
-        cellRendererParams: {}, // 空对象，无需传递参数
-        width: 150,
-      },
-      {
-        headerName: "編號",
-        field: "AI_ID",
-        unSortIcon: true,
-        sortable: true,
-        width: 150,
-        suppressMovable: true
-      },
-      {
-        headerName: "設備總類",
-        field: "EquipTypeName",
-        unSortIcon: true,
-        sortable: true,
-        width: 150,
-        suppressMovable: true
-      },
-      {
-        headerName: "設備分類",
-        field: "EquipCategoryName",
-        unSortIcon: true,
-        sortable: true,
-        width: 150,
-        suppressMovable: true
-      },
-      // {
-      //   headerName: "資產編號",
-      //   field: "AssetsId",
-      //   unSortIcon: true,
-      //   sortable: true,
-      //   width: 150,
-      //   suppressMovable: true
-      // },
-      {
-        headerName: "物品名稱",
-        field: "AssetName",
-        unSortIcon: true,
-        sortable: true,
-        width: 250,
-        suppressMovable: true
-      },
-      {
-        headerName: "狀態",
-        field: "Status",
-        unSortIcon: true,
-        sortable: true,
-        width: 100,
-        suppressMovable: true
-      },
-      // {
-      //   headerName: "區域",
-      //   field: "AreaName",
-      //   unSortIcon: true,
-      //   sortable: true,
-      //   width: 100,
-      //   suppressMovable: true
-      // },
-      // {
-      //   headerName: "櫃位",
-      //   field: "LayerName",
-      //   unSortIcon: true,
-      //   sortable: true,
-      //   width: 100,
-      //   suppressMovable: true,
-      //   suppressMovable: true
-      // },
-      {
-        headerName: "申請入庫日期",
-        field: "ApplicationDate",
-        unSortIcon: true,
-        sortable: true,
-        width: 200,
-        suppressMovable: true
-      },
-      {
-        headerName: "申請人員",
-        field: "Applicant",
-        unSortIcon: true,
-        sortable: true,
-        width: 180,
-        suppressMovable: true
-      },
-      {
-        field: "",
-        cellRenderer: "Delete_button",
-      }
-    ];
-    const rowData = ref([
-      // {
-      //   AI_ID: 'S202300001',
-      //   EquipTypeName: "Type A",
-      //   EquipCategoryName: "Category X",
-      //   AssetsId: "A123",
-      //   AssetName: "Asset 1",
-      //   Status: "Active",
-      //   AreaName: "Area 1",
-      //   LayerName: "Layer 1",
-      //   ApplicationDate: "2023/08/14",
-      //   Applicant: "John Doe"
-      // },
-    ]);
-    async function submit() {
-      const formData = new FormData();
-      const formFields = {
-        'EquipTypeName': EquipTypeName.value,
-        'EquipCategoryName': EquipCategoryName.value,
-        'AssetName': AssetName.value,
-        'Status': Status.value,
-        'StartDate': StartDate.value,
-        'EndDate': EndDate.value,
-      };
-      //將表格資料append到 formData
-      for (const fieldName in formFields) {
-        formData.append(fieldName, formFields[fieldName]);
-        // console.log(formData.get(`${fieldName}`));
-      }
-      //使用axios method:post傳送新品入庫表單
-      const axios = require('axios');
-      try {
-        const response = await axios.post('http://192.168.0.176:7008/AssetsInMng/Applications', formData, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-        });
-        console.log(response);
-        const data = response.data;
-        if (data.state === 'success') {
-          //取得datagrid成功
-          // console.log(data.state);
-          console.log(data.messages);
-          console.log('datagrid', data.resultList);
-          rowData.value = data.resultList;
-        } else if (data.state === 'error') {
-          //取得datagrid失敗
-          alert(data.messages);
+  import {
+    ref,
+    onMounted
+  } from "vue";
+  import {
+    AgGridVue
+  } from "ag-grid-vue3";
+  import Storage_return_button from "@/components/Storage_return_button";
+  import Delete_button from "@/components/Delete_button";
+  import Navbar from "@/components/Navbar.vue";
+  import router from "@/router";
+  export default {
+    components: {
+      Navbar,
+      AgGridVue,
+      Storage_return_button,
+      Delete_button,
+    },
+    setup() {
+      const EquipTypeName = ref(''); //設備總類 *必填
+      const EquipTypeArray = ref([]); //設備總類陣列 request拿到
+      const EquipCategoryName = ref(''); //設備分類 *必填
+      const EquipCategoryArray = ref([]); //設備分類陣列 request拿到
+      const EquipCategoryInit = ref('請先選擇設備總類');
+      // const AssetId = ref(''); //資產編號
+      const AssetName = ref(''); //物品名稱
+      const Status = ref(''); //狀態
+      const StatusArray = ref(['申請入庫', '申請歸還', '可交付', '待入庫', '已入庫', '已歸還', ])
+      const StartDate = ref(''); //申請入庫日期(起)
+      const EndDate = ref(''); //申請入庫日期(迄)
+      const total = 100;
+      const pageSize = 10;
+      const pagePosition = ref("bottom");
+      const pageOptions = [{
+          value: "bottom",
+          text: "Bottom"
+        },
+        {
+          value: "top",
+          text: "Top"
+        },
+        {
+          value: "both",
+          text: "Both"
         }
       ];
       const columnDefs = [{
@@ -490,8 +356,9 @@ export default {
       }
       function selectType(item) {
         EquipTypeName.value = item;
-        console.log('選擇的總類:', EquipTypeName.value);
+        // console.log('選擇的總類:', EquipTypeName.value);
         getEquipCategoryName();
+        EquipCategoryInit.value = '請選擇';
       }
       function selectCategory(item) {
         EquipCategoryName.value = item;
@@ -502,6 +369,7 @@ export default {
       const clear = () => {
         EquipTypeName.value = '';
         EquipCategoryName.value = '';
+        EquipCategoryInit.value = '請先選擇設備總類';
         AssetName.value = '';
         Status.value = '';
         StartDate.value = '';
@@ -520,6 +388,7 @@ export default {
         getEquipTypeName,
         EquipCategoryName,
         EquipCategoryArray,
+        EquipCategoryInit,
         getEquipCategoryName,
         // AssetId,
         AssetName,
@@ -541,66 +410,7 @@ export default {
         frameworkComponents,
       };
     }
-    function selectType(item) {
-      EquipTypeName.value = item;
-      // console.log('選擇的總類:', EquipTypeName.value);
-      getEquipCategoryName();
-      EquipCategoryInit.value = '請選擇';
-    }
-    function selectCategory(item) {
-      EquipCategoryName.value = item;
-    }
-    const selectStatus = (item) => {
-      Status.value = item;
-    };
-
-    const clear = () => {
-      EquipTypeName.value = '';
-      EquipCategoryName.value = '';
-      EquipCategoryInit.value = '請先選擇設備總類';
-      AssetName.value = '';
-      Status.value = '';
-      StartDate.value = '';
-      EndDate.value = '';
-      submit();
-    };
-    const frameworkComponents = {
-      agGridVue: AgGridVue
-    };
-
-    onMounted(() => {
-      submit();
-    });
-
-    return {
-      EquipTypeName,
-      EquipTypeArray,
-      getEquipTypeName,
-      EquipCategoryName,
-      EquipCategoryArray,
-      EquipCategoryInit,
-      getEquipCategoryName,
-      // AssetId,
-      AssetName,
-      Status,
-      StatusArray,
-      StartDate,
-      EndDate,
-      total,
-      pageSize,
-      pagePosition,
-      pageOptions,
-      selectType,
-      selectCategory,
-      selectStatus,
-      submit,
-      clear,
-      columnDefs,
-      rowData,
-      frameworkComponents,
-    };
-  }
-};
+  };
 </script>
 
 <style lang="scss" scoped>
@@ -646,7 +456,7 @@ export default {
       .datagrid_section {
         .row {
           gap: 20px 50px;
-          padding: 40px 125px;
+          padding: 40px 0 40px 16%;
           @include datagrid_bg;
           p {
             @include datagrid_title;
@@ -671,7 +481,9 @@ export default {
               display: flex;
               justify-content: space-between;
               align-items: center;
-              border: none;
+              &::after {
+                font-size: 20px; // margin-left: 60%;
+              }
             }
             .dropdown-menu {
               width: 100%;
@@ -759,11 +571,9 @@ export default {
             width: 100%;
             height: 35px;
             @include dropdown_btn;
-            .dropdown-toggle {
-              display: flex;
-              justify-content: space-between;
-              align-items: center;
-              border: none;
+            .dropdown-toggle::after {
+              font-size: 20px;
+              margin-left: 75%;
             }
             .dropdown-menu {
               width: 100%;
@@ -862,11 +672,9 @@ export default {
             width: 100%;
             height: 35px;
             @include dropdown_btn;
-            .dropdown-toggle {
-              display: flex;
-              justify-content: space-between;
-              align-items: center;
-              border: none;
+            .dropdown-toggle::after {
+              font-size: 20px;
+              margin-left: 75%;
             }
             .dropdown-menu {
               width: 100%;
