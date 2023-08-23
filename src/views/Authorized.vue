470453bdea20010b1a85f12819e3ea6fcbffdcbd
margo <minmm98@gmail.com>
2023-08-23 14:16:19 +0800

    新增權限錯誤頁面

---
+++ src/views/Authorized.vue
@@ -0,0 +1,69 @@
+<template>
+  <Navbar/>
+  <div class="main_section">
+  <div class="title col">
+      <h1>注意:您未具備本項操作權限!</h1>
+     
+    </div>
+     <div class="text-center">
+        <img src="../assets/authorized.png" alt="權限不足">
+      </div>
+      </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue';
+export default{
+  components:{
+    Navbar
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "@/assets/css/global.scss";
+
+  @media only screen and (min-width: 1200px){
+    .main_section{
+   
+    
+    h1 {
+        margin-top: 150px;
+        text-align: center;
+        font-size: 55px;
+        font-weight: 600;
+      color:#c92626
+      }}
+}
+
+
+@media only screen and (min-width: 768px) and (max-width: 1199px){
+  .main_section{
+   
+    
+   h1 {
+       margin-top: 150px;
+       text-align: center;
+       font-size: 55px;
+       font-weight: 600;
+     color:#c92626
+     }}
+}
+
+@media only screen and (max-width: 767px){
+  .main_section{
+   
+    
+   h1 {
+   padding:0 5%;
+       margin-top: 80px;
+       text-align: center;
+       font-size: 40px;
+       font-weight: 600;
+     color:#c92626
+     }}
+}
+
+
+
+</style>
