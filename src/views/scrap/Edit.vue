01dd6809677a39bae48d089e09dded7db5bdf7f2
margo <minmm98@gmail.com>
2023-09-26 16:03:35 +0800

    編輯報廢

---
+++ src/views/scrap/Edit.vue
@@ -0,0 +1,523 @@
+<template>
+  <Navbar/>
+  <div class="main_section">
+    <div class="title col">
+      <h1>
+        編輯報廢
+      </h1>
+    </div>
+    <div class="info_wrap col">
+      <!-- 報廢編號，申請人員，申請日期 -->
+      <div class="fixed_info">
+        <div>
+          <p>
+            報廢編號: {{ Applicant }}
+          </p>
+        </div>
+        <div>
+          <p>
+            申請人員: {{ Applicant }}
+          </p>
+        </div>
+        <div>
+          <p>
+            申請日期: {{ ApplicationDate }}
+          </p>
+        </div>
+      </div>
+      <div class="content">
+        <div class="row g-0">
+          <!-- 報廢人員 -->
+          <div class="col-xl-6 col-lg-6 col-md-6 col-12">
+            <div class="input-group" :class="{'mb-4': !wrongStatus}">
+              <div class="input-group-prepend">
+                報廢人員：
+              </div>
+              <input ref="inputElement" type="text" class="form-control readonly_box" readonly>
+            </div>
+          </div>
+          <!-- 交付日期 -->
+          <div class="col-xl-6 col-lg-6 col-md-6 col-12">
+            <div class="input-group" :class="{'mb-4': !wrongStatus}">
+              <div class="input-group-prepend">
+                交付日期：
+              </div>
+              <input ref="inputElement" type="text" class="form-control readonly_box" readonly>
+            </div>
+          </div>
+        </div>
+        <div class="row g-0">
+          <!-- 審核人員 -->
+          <div class="col-xl-6 col-lg-6 col-md-6 col-12">
+            <div class="input-group" :class="{'mb-4': !wrongStatus}">
+              <div class="input-group-prepend">
+                審核人員：
+              </div>
+              <input ref="inputElement" type="text" class="form-control readonly_box" readonly>
+            </div>
+          </div>
+          <!-- 審核結果 -->
+          <div class="col-xl-6 col-lg-6 col-md-6 col-12">
+            <div class="input-group" :class="{'mb-4': !wrongStatus}">
+              <div class="input-group-prepend">
+                審核結果：
+              </div>
+              <input ref="inputElement" type="text" class="form-control readonly_box" readonly>
+            </div>
+          </div>
+        </div>
+        <!-- 審核日期 -->
+        <div class="col-xl-6 col-lg-6 col-md-6 col-12">
+          <div class="input-group d-flex mb-4">
+            <div class="input-group-prepend">
+              審核日期：
+            </div>
+            <input ref="inputElement" type="text" class="form-control readonly_box" readonly>
+          </div>
+        </div>
+        <!-- 產編 -->
+        <div class="col-12">
+          <div class="input-group" :class="{'mb-4': !wrongStatus}">
+            <div class="input-group-prepend">
+              <span>*</span>產編：
+            </div>
+            <input ref="inputElement" type="text" class="form-control" placeholder="請掃描輸入產編">
+            <button class="form_search_btn">搜尋</button>
+          </div>
+        </div>
+        <!-- 物品名稱 -->
+        <div class="col-12">
+          <div class="input-group" :class="{'mb-4': !wrongStatus}">
+            <div class="input-group-prepend">
+              物品名稱：
+            </div>
+            <input ref="inputElement" type="text" class="form-control readonly_box" readonly>
+          </div>
+        </div>
+        <!-- 報廢原因 -->
+        <div class="col-12">
+          <div class="input-group d-flex">
+            <div class="input-group-prepend">
+              報廢原因：
+            </div>
+            <textarea style="height: 200px;" class="form-control" placeholder="最多輸入500字"></textarea>
+          </div>
+        </div>
+     
+      </div>
+      <div class="col button_wrap">
+        <button class="back_btn" @click="goBack">回上一頁</button>
+        <button class="send_btn" :disabled="!canSubmit" :class="{send_btn_disabled: !canSubmit}" @click="submit">送出</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import {
+    ref,
+    onMounted,
+    reactive
+  } from 'vue';
+  import Navbar from '@/components/Navbar.vue';
+  import router from '@/router';
+  export default {
+    components: {
+      Navbar
+    },
+  };
+</script>
+
+
+<style lang="scss" scoped>
+  @import '@/assets/css/global.scss';
+  @media only screen and (min-width: 1200px) {
+    .main_section {
+      .readonly_box {
+        @include readonly_box;
+        font-weight: 500;
+      }
+      .form_search_btn {
+        @include form_search_btn;
+      }
+      h1 {
+        margin-top: 100px;
+        text-align: center;
+        font-size: 55px;
+        font-weight: 600;
+        @include title_color;
+      }
+      .info_wrap {
+        margin: 30px auto 5%;
+        width: 750px;
+        .fixed_info {
+          @include fixed_info;
+          p {
+            font-size: 20px;
+            margin-bottom: 0;
+          }
+        }
+        .content {
+          @include content_bg;
+          .input-group {
+            .input-number {
+              @include count_btn;
+            }
+            .readonly_box {
+              text-align: center;
+              height: 37px;
+            }
+            .form-control {
+              height: 37px;
+              border-radius: 0;
+            }
+            .input-group-prepend {
+              color: white;
+              font-weight: 700;
+              font-size: 20px;
+              text-align: end;
+              width: 155px;
+              span {
+                @include red_star
+              }
+            }
+            .selected_file {
+              margin-left: 20px;
+              p.title {
+                font-weight: 700;
+                color: white;
+                margin-bottom: 5px;
+              }
+              .file_upload_wrap {
+                margin-bottom: 0;
+                display: flex;
+                img {
+                  width: 25px;
+                  height: 25px;
+                }
+                p {
+                  margin-bottom: 0;
+                  font-weight: 700;
+                  color: white;
+                  &::before {
+                    margin-right: 10px;
+                    content: '·';
+                    font-weight: 700;
+                    color: white;
+                  }
+                }
+              }
+            }
+            .file_wrap {
+              display: flex;
+              flex-direction: column;
+              .choose_btn {
+                margin-bottom: 10px;
+                @include choose_file_btn;
+                &:hover {
+                  background: #3f608f;
+                }
+              }
+            }
+          }
+        }
+        .button_wrap {
+          display: flex;
+          justify-content: space-between;
+          margin: 30px auto 5%;
+          width: 220px;
+          button {
+            &:nth-child(1) {
+              @include back_to_previous_btn;
+              &:hover {
+                background-color: #5d85bb;
+              }
+            }
+          }
+          .send_btn {
+            @include search_and_send_btn;
+            &:hover {
+              background-color: #5e7aa2;
+            }
+          }
+          .send_btn_disabled {
+            background: #878787;
+            &:hover {
+              background: #878787;
+            }
+          }
+        }
+      }
+    }
+  }
+  @media only screen and (min-width: 768px) and (max-width: 1199px) {
+    .main_section {
+      .readonly_box {
+        @include readonly_box;
+      }
+      .form_search_btn {
+        @include form_search_btn;
+      }
+      h1 {
+        margin-top: 100px;
+        text-align: center;
+        font-size: 55px;
+        font-weight: 600;
+        @include title_color;
+      }
+      .info_wrap {
+        margin: 30px auto 5%;
+        padding: 0 5%;
+        .fixed_info {
+          @include fixed_info;
+          p {
+            font-size: 20px;
+            margin-bottom: 0;
+          }
+        }
+        .content {
+          @include content_bg;
+          .input-group {
+            width: 100%;
+            white-space: nowrap;
+            flex-wrap: nowrap;
+            .input-number {
+              width: 100%;
+              @include count_btn;
+            }
+            .readonly_box {
+              height: 37px;
+              width: 100%;
+            }
+            .form-control {
+              height: 37px;
+              width: 65%;
+            }
+            .input-group-prepend {
+              color: white;
+              font-weight: 700;
+              font-size: 20px;
+              text-align: end;
+              width: 150px;
+              span {
+                @include red_star
+              }
+            }
+          }
+          .repair_photo_section {
+            .selected_file {
+              margin-left: 20px;
+              p.title {
+                font-weight: 700;
+                color: white;
+                margin-bottom: 5px;
+              }
+              .file_upload_wrap {
+                margin-bottom: 0;
+                display: flex;
+                img {
+                  width: 25px;
+                  height: 25px;
+                }
+                p {
+                  margin-bottom: 0;
+                  font-weight: 700;
+                  color: white;
+                  &::before {
+                    margin-right: 10px;
+                    content: '·';
+                    font-weight: 700;
+                    color: white;
+                  }
+                }
+              }
+            }
+            .file_wrap {
+              display: flex;
+              flex-direction: column;
+              .choose_btn {
+                margin-bottom: 10px;
+                @include choose_file_btn;
+                &:hover {
+                  background: #3f608f;
+                }
+              }
+            }
+          }
+        }
+        .button_wrap {
+          display: flex;
+          justify-content: space-between;
+          margin: 30px auto 5%;
+          width: 220px;
+          button {
+            &:nth-child(1) {
+              @include back_to_previous_btn;
+              &:hover {
+                background-color: #5d85bb;
+              }
+            }
+          }
+          .send_btn {
+            @include search_and_send_btn;
+            &:hover {
+              background-color: #5e7aa2;
+            }
+          }
+          .send_btn_disabled {
+            background: #878787;
+            &:hover {
+              background: #878787;
+            }
+          }
+        }
+      }
+    }
+  }
+  @media only screen and (max-width: 767px) {
+    .main_section {
+      .readonly_box {
+        @include readonly_box;
+        height: 35px;
+        text-align: center;
+        margin-left: unset !important;
+      }
+      .form_search_btn {
+        border: none;
+        color: white;
+        width: 60px;
+        height: 35px;
+        margin-left: unset !important;
+        margin-top: 10px;
+        font-weight: 700;
+        padding: 0 10px;
+        background-color: #132238;
+        &:hover {
+          background-color: #43546d;
+        }
+      }
+      h1 {
+        margin-top: 80px;
+        text-align: center;
+        font-size: 40px;
+        font-weight: 600;
+        @include title_color;
+      }
+      .info_wrap {
+        padding: 0 5%;
+        .fixed_info {
+          @include fixed_info;
+          height: unset;
+          flex-direction: column;
+          padding: 10px;
+          p {
+            font-size: 20px;
+            margin-bottom: 0;
+          }
+        }
+        .content {
+          @include content_bg;
+          .input-group {
+            flex-direction: column;
+            .input-group> :not(:first-child):not(.dropdown-menu):not(.valid-tooltip):not(.valid-feedback):not(.invalid-tooltip):not(.invalid-feedback) {
+              margin-left: unset;
+              border-radius: 5px;
+              margin-top: 5px;
+              height: 35px;
+            }
+            .input-number {
+              @include count_btn;
+              margin-left: unset !important;
+            }
+            .form-control {
+              height: 35px;
+              width: 100%;
+              border-radius: 0;
+              margin-left: unset !important;
+            }
+            .input-group-prepend {
+              color: white;
+              font-weight: 700;
+              font-size: 20px;
+              width: 100px;
+              white-space: nowrap;
+              span {
+                @include red_star
+              }
+            }
+            .selected_file {
+              p.title {
+                font-weight: 700;
+                color: white;
+                margin-bottom: 5px;
+              }
+              .file_upload_wrap {
+                margin-bottom: 0;
+                display: flex;
+                img {
+                  width: 25px;
+                  height: 25px;
+                }
+                p {
+                  margin-bottom: 0;
+                  font-weight: 700;
+                  color: white;
+                  &::before {
+                    margin-right: 10px;
+                    content: '·';
+                    font-weight: 700;
+                    color: white;
+                  }
+                }
+              }
+            }
+            .file_wrap {
+              display: flex;
+              flex-direction: column;
+              margin-left: unset !important;
+              .choose_btn {
+                margin-top: 5px;
+                margin-bottom: 10px;
+                @include choose_file_btn;
+                &:hover {
+                  background: #3f608f;
+                }
+              }
+            }
+          }
+        }
+        .button_wrap {
+          display: flex;
+          justify-content: space-between;
+          margin: 30px auto 5%;
+          width: 190px;
+          button {
+            &:nth-child(1) {
+              @include back_to_previous_btn;
+              width: 100px;
+              &:hover {
+                background-color: #5d85bb;
+              }
+            }
+          }
+          .send_btn {
+            @include search_and_send_btn;
+            width: 70px;
+            padding: 5px;
+            &:hover {
+              background-color: #5e7aa2;
+            }
+          }
+          .send_btn_disabled {
+            background: #878787;
+            width: 70px;
+            padding: 5px;
+            &:hover {
+              background: #878787;
+            }
+          }
+        }
+      }
+    }
+  }
+</style>
