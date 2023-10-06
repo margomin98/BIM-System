f10f4fff60765ccbafdaa82acff16a97869d8fe6
margo <minmm98@gmail.com>
2023-10-06 16:50:28 +0800

    系統日誌router

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -61,7 +61,7 @@
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
               <router-link to="/authority">權限管理</router-link>
               <router-link to="/system_parameter">參數管理</router-link>
-              <router-link to="">系統日誌管理</router-link>
+              <router-link to="/system_log_datagrid">系統日誌管理</router-link>
             </div>
           </li>
         </ul>
