5d04360ddc0683529923b6492abff034e9ce66b2
nyxxxxx <s0941212@gmail.com>
2023-10-27 09:16:02 +0800

    Update View.vue

--- src/views/store/View.vue
+++ src/views/store/View.vue
@@ -220,7 +220,8 @@
                 <div class="input-group-prepend">已上傳檔案 :</div>
                 <div class="selected_file">
                   <div v-for="(file , file_index) in tab.existFile" :key="file_index" class="file_upload_wrap" style="cursor: pointer;">
-                    <p @click="viewImgFile(index , file_index)" data-bs-toggle="modal" data-bs-target="#viewFile_modal">{{ file.FileName }}</p>
+                    <p data-bs-toggle="modal" data-bs-target="#viewFile_modal">{{ file.FileName }}</p>
+                    <img class="view_icon" src="@/assets/view.png" style="margin-left: 10px;" @click="viewImgFile()" data-bs-toggle="modal" data-bs-target="#viewFile_modal">
                   </div>
                 </div>
               </div>
