4d57dd507f096dd6b593a7100167d2dbd046f176
margo <minmm98@gmail.com>
2023-12-19 14:47:00 +0800

    新增訂購。編輯訂單，檢視訂單，刪除訂單,訂單navbar

---
+++ src/views/order/Datagrid.vue
@@ -0,0 +1,390 @@
+<template>
+  <Navbar />
+  <div class="main_section">
+    <div class="title col">
+      <h1>訂單管理</h1>
+    </div>
+    <div class="col">
+      <div class="button_wrap d-flex">
+        <router-link to="/order_new">
+          <button class="add_btn">新增訂單</button>
+        </router-link>
+      </div>
+    </div>
+    <div class="container-fluid datagrid_section">
+      <div class="content">
+        <div class="row">
+          <!-- 狀態 -->
+          <div class="col-xl-auto col-lg-auto col-md-auto col-12">
+            <p>狀態</p>
+            <div class="dropdown">
+              <button class="btn dropdown-toggle" type="button" id="activeDropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        {{  searchParams.Active ||"請選擇" }}
+                      </button>
+              <div class="dropdown-menu" aria-labelledby="activeDropdown">
+                <p :key="index" class="dropdown-item" @click="selectActive(item)">{{ item }}</p>
+              </div>
+            </div>
+          </div>
+          <!-- 使用專案 -->
+          <div class="col-xl-auto col-lg-auto col-md-auto col-12">
+            <p>使用專案</p>
+            <input type="text" v-model="searchParams.Message" />
+          </div>
+          <!-- 採購來源 -->
+          <div class="col-xl-auto col-lg-auto col-md-auto col-12">
+            <p>採購來源</p>
+            <input type="text" v-model="searchParams.Message" />
+          </div>
+          <!-- 下訂日期(起) -->
+          <div class="col-xl-auto col-lg-auto col-md-auto col-12">
+            <p>下訂日期(起)</p>
+            <input type="datetime-local" class="date-input" v-model="searchParams.StartDate" />
+          </div>
+          <!-- 下訂日期(迄) -->
+          <div class="col-xl-auto col-lg-auto col-md-auto col-12">
+            <p>下訂日期(迄)</p>
+            <input type="datetime-local" class="date-input" v-model="searchParams.EndDate" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col justify-content-center d-flex">
+      <div class="button_wrap d-flex">
+        <button class="search_btn" @click="submit('','search')">檢索</button>
+        <button class="empty_btn" @click="clear">清空</button>
+        <!-- <button class="export_btn">匯出</button> -->
+      </div>
+    </div>
+    <div class="dg-height mb-5">
+      <DataTable
+        lazy
+        :key="datagrid.key"
+        :first= "datagrid.first"
+        :size="'small'"
+        :loading="datagrid.loading"
+        :value="rowData" 
+        :sort-field="datagrid.sortField"
+        :sort-order="datagrid.sortOrder"
+        resizableColumns 
+        columnResizeMode="expand"
+        showGridlines 
+        scrollable 
+        scrollHeight="420px" 
+        @page="submit($event , 'page')" 
+        @sort="submit($event , 'sort')"
+        paginator 
+        :rows="datagrid.rows" 
+        :totalRecords="datagrid.totalRecords"
+        paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
+        :rowsPerPageOptions="[10, 20, 30]"
+        currentPageReportTemplate=" 第{currentPage}頁 ，共{totalPages}頁 總筆數 {totalRecords}">
+        <Column style="min-width: 60px;">
+          <template #body="slotProps">
+            <Receive_button :params = "slotProps"/>
+          </template>
+        </Column>
+        <Column v-for="item in datagridfield" :field="item.field" :header="item.header" sortable :style="{'min-width': item.width}"></Column>
+        <Column style="min-width: 60px;">
+          <template #body="slotProps">
+            <Delete :params = "slotProps"/>
+          </template>
+        </Column>
+      </DataTable>
+    </div>
+  </div>
+</template>
+
+<script>
+  import DataTable from 'primevue/datatable';
+  import Column from 'primevue/column';
+  import Receive_button from "@/components/Receive_button";
+  import Delete from "@/components/Receive_delete_button";
+  import Navbar from "@/components/Navbar.vue";
+  import {
+    onMounted,
+    reactive,
+    ref
+  } from "vue";
+  import {
+    useRouter
+  } from "vue-router";
+  import { UpdatePageParameter, createDatagrid } from '@/assets/js/common_fn';
+  import axios from 'axios';
+import { getMngDatagrid } from '@/assets/js/common_api';
+  export default {
+    components: {
+      Navbar,
+      DataTable,
+      Column,
+      Receive_button,
+      Delete
+    },
+    setup() {
+      const router = useRouter();
+      const searchParams = reactive({
+        ShipmentNum: '',
+        ShipmentCompany: '',
+        StartDate: '',
+        EndDate: '',
+      });
+      const datagrid = createDatagrid();
+      const datagridfield = [
+        { header: "單號", field: "Show_AR_ID", width: '150px' },
+        { header: "狀態", field: "ShipmentNum", width: '130px' },
+        { header: "訂單編號", field: "ShipmentCompany", width: '150px' },
+        { header: "使用專案", field: "GoodsNum", width: '180px' },
+        { header: "採購來源", field: "ReceivedDate", width: '150px' },
+        { header: "採購件數", field: "Recipient", width: '130px' },
+        { header: "下訂日期", field: "Recipient", width: '130px' },
+        { header: "承辦人員", field: "Recipient", width: '130px' },
+      ]
+      const rowData = ref([]);
+      onMounted(() => {
+        datagrid.sortField = 'AR_ID'
+        submit('','search');
+      });
+      async function submit(event,type) {
+        const form = new FormData();
+        //將表格資料append到 form
+        for (const key in searchParams) {
+          if (searchParams[key]) {
+            form.append(key, searchParams[key]);
+          }
+        }
+        UpdatePageParameter(datagrid,event,type,form)
+        getMngDatagrid('/ReceivingMng/ReceivingNotes',rowData,datagrid,form);
+      }
+      const clear = () => {
+        for (const key in searchParams) {
+          searchParams[key] = '';
+        }
+        submit('','search');
+      }
+      return {
+        searchParams,
+        datagrid,
+        datagridfield,
+        rowData,
+        submit,
+        clear,
+      };
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import "@/assets/css/global.scss";
+  @media only screen and (min-width: 1200px) {
+    .main_section {
+      padding: 0 10%;
+      h1 {
+        margin: 50px 0 20px;
+        text-align: center;
+        font-size: 55px;
+        font-weight: 600;
+        @include title_color;
+      }
+      .button_wrap {
+        margin-bottom: 25px;
+        gap: 20px;
+        .add_btn {
+          @include datagrid_button_no1;
+          &:hover {
+            background-color: #537ebc;
+          }
+        }
+        .search_btn {
+          @include search_and_send_btn;
+          &:hover {
+            background-color: #5e7aa2;
+          }
+        }
+        .empty_btn {
+          @include empty_btn;
+          &:hover {
+            background-color: #5d85bd;
+          }
+        }
+       
+      }
+      .datagrid_section {
+        .content {
+          background: rgba(82, 136, 156, 0.8);
+          border-radius: 10px;
+          margin-bottom: 30px;
+          height: 240px;
+          align-items: center;
+          display: flex;
+          justify-content: center;
+        }
+        .row {
+          display: grid;
+          grid-template-rows: 1fr 1fr;
+          grid-template-columns: 1fr 1fr 1fr;
+          gap: 40px 5px;
+          p {
+            @include datagrid_title;
+          }
+          input {
+            @include dropdown_btn;
+            width: 240px;
+            height: 35px;
+          }
+          button {
+            border: none;
+            padding: 0;
+            width: 100%;
+            font-size: 18px;
+            height: 100%;
+          }
+          .dropdown {
+            width: 240px;
+            height: 35px;
+            @include dropdown_btn;
+            .dropdown-toggle {
+              display: flex;
+              justify-content: space-between;
+              align-items: center;
+              border: none;
+            }
+            .dropdown-menu {
+              width: 100%;
+              transform: translate3d(-1px, 35px, 0px) !important;
+              p {
+                font-size: 18px;
+                color: black;
+                font-weight: normal;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  @media only screen and (min-width: 768px) and (max-width: 1199px) {
+    .main_section {
+      padding: 0 5%;
+      h1 {
+        margin-top: 30px;
+        text-align: center;
+        font-size: 55px;
+        font-weight: 600;
+        @include title_color;
+        margin-bottom: 20px;
+      }
+      .button_wrap {
+        margin-bottom: 25px;
+        gap: 20px;
+        .add_btn {
+          @include datagrid_button_no1; 
+          &:hover {
+            background-color: #537ebc;
+          }
+        }
+    
+        .search_btn {
+          @include search_and_send_btn;
+          &:hover {
+            background-color: #5e7aa2;
+          }
+        }
+        .empty_btn {
+          @include empty_btn;
+          &:hover {
+            background-color: #5d85bd;
+          }
+        }
+      }
+      .datagrid_section {
+        .row {
+          gap: 10px 0;
+          padding: 30px;
+          @include datagrid_bg;
+          p {
+            @include datagrid_title;
+          }
+          input {
+            @include dropdown_btn;
+            width: 100%;
+            height: 35px;
+          }
+          button {
+            padding: 0;
+            width: 100%;
+            font-size: 18px;
+            height: 100%;
+            text-align: left;
+          }
+         
+        }
+      }
+    }
+  }
+  @media only screen and (max-width: 767px) {
+    .main_section {
+      padding: 5%;
+      h1 {
+        margin-top: 30px;
+        text-align: center;
+        font-size: 50px;
+        font-weight: 600;
+        @include title_color;
+        margin-bottom: 20px;
+      }
+      .button_wrap {
+        margin-bottom: 25px;
+        justify-content: center;
+        gap: 20px;
+        .add_btn {
+          @include datagrid_button_no1;
+          font-size: 18px;
+          width: 100%;
+          height: auto;
+          &:hover {
+            background-color: #537ebc;
+          }
+        }
+    
+        .search_btn {
+          @include search_and_send_btn;
+          &:hover {
+            background-color: #5e7aa2;
+          }
+        }
+        .empty_btn {
+          @include empty_btn;
+          &:hover {
+            background-color: #5d85bd;
+          }
+        }
+      }
+      .datagrid_section {
+        .row {
+          display: flex;
+    flex-direction: column;
+          gap: 10px 0;
+          padding: 30px;
+          @include datagrid_bg;
+          p {
+            @include datagrid_title;
+            font-size: 18px;
+          }
+          input {
+            @include dropdown_btn;
+            width: 100%;
+            height: 35px;
+          }
+          button {
+            padding: 0;
+            width: 100%;
+            font-size: 18px;
+            height: 100%;
+            text-align: left;
+          }
+         
+        }
+      }
+    }
+  }
+</style>
---
+++ src/views/order/Delete.vue
@@ -0,0 +1,338 @@
+<template>
+  <Navbar/>
+  <div class="main_section">
+    <div class="title col">
+      <h1>
+        刪除項目
+      </h1>
+    </div>
+    <Order_component/>
+    <div class="col button_wrap">
+      <button class="back_btn" @click="goBack">回上一頁</button>
+      <button class="delete_btn" data-bs-toggle="modal" data-bs-target="#deleteModal">刪除</button> </div>
+  </div>
+</template>
+
+<script>
+  import {
+    onMounted,
+    reactive,
+    ref,
+    watch
+  } from 'vue';
+  import Navbar from '@/components/Navbar.vue';
+  import Order_component from '@/components/Order_component.vue';
+  import router from '@/router';
+  import {
+    getDate,
+    goBack,
+    checkFileSize,
+  } from '@/assets/js/common_fn.js'
+  import {
+    getApplication,
+    getAssets
+  } from '@/assets/js/common_api.js'
+  import axios from 'axios'
+  export default {
+    components: {
+      Navbar,
+      Order_component
+    },
+    setup() {
+      const Applicant = ref('');
+      const ApplicationDate = ref('');
+      const Assets = reactive({
+        Name: '',
+        Type: '',
+        Status: '',
+      });
+      const formParams = reactive({
+        AssetsId: '',
+        Question: '',
+        newFile: [],
+        viewFile: [], //不需要傳
+      });
+      const modalParams = reactive({
+        title: '',
+        src: '',
+      })
+      const alertMsg = ref('');
+      const wrongStatus = ref(false);
+      const canSubmit = ref(false);
+      const fileInputs = ref(null);
+      onMounted(() => {
+        getApplicationInfo()
+        ApplicationDate.value = getDate()
+      });
+      async function getApplicationInfo() {
+        getApplication()
+          .then((data) => {
+            Applicant.value = data;
+          })
+          .catch((error) => {
+            console.error(error);
+          })
+      }
+      // async function getAssetsInfo() {
+      //   getAssets(formParams.AssetsId)
+      //     .then((data)=>{
+      //       Assets.Name = data.AssetName;
+      //       Assets.Type = data.AssetType;
+      //       Assets.Status = data.Status;
+      //       // 檢查資產類型
+      //       if(Assets.Type === '耗材') {
+      //         wrongStatus.value = true;
+      //         canSubmit.value = false;
+      //         alertMsg.value = '僅提供資產類型為非耗材的物品進行維修'
+      //       }
+      //       // 檢查資產狀態(只有非耗材才會有這個Status)
+      //       else if(Assets.Status === '已被設備整合') {
+      //         wrongStatus.value = true;
+      //         canSubmit.value = false;
+      //         alertMsg.value = '此資產已被設備整合，請先移出設備箱'
+      //       }
+      //       // 可報修
+      //       else {
+      //         wrongStatus.value = false;
+      //         canSubmit.value = true;
+      //         alertMsg.value = ''
+      //       }
+      //     })
+      //     .catch((error) =>{
+      //       wrongStatus.value = true;
+      //       canSubmit.value = false;
+      //       Assets.Name = '';
+      //       alertMsg.value = '請輸入正確的資產編號'
+      //     })
+      // }
+      async function submit() {
+        const pattern = /^(BF\d{8})$/;
+        // 檢查必填項目、格式        
+        if (!pattern.test(formParams.AssetsId)) {
+          alert('資產編號格式錯誤');
+          return
+        }
+        if (!/^[\s\S]{0,500}$/.test(formParams.Question)) {
+          alert('問題描述不可超過500字');
+          return
+        }
+        const form = new FormData();
+        for (const key in formParams) {
+          if (formParams[key]) {
+            form.append(key, formParams[key]);
+          }
+        }
+        // 移除viewFile
+        form.delete('viewFile');
+        // newFile額外append
+        form.delete('newFile');
+        for (let i = 0; i < formParams.newFile.length; i++) {
+          form.append('newFile', formParams.newFile[i]);
+        }
+        axios.post('http://192.168.0.177:7008/RepairMng/CreateOrder', form)
+          .then((response) => {
+            const data = response.data;
+            if (data.state === 'success') {
+              alert('新增報修單成功\n單號為:' + data.resultList.R_ID);
+              router.push({
+                name: 'Repair_Datagrid'
+              });
+            } else if (data.state === 'account_error') {
+              alert(data.messages);
+              router.push('/');
+            } else {
+              alert('新增報修單失敗')
+            }
+          })
+          .catch((error) => {
+            console.error(error);
+          })
+      }
+      const openFileExplorer = (() => {
+        fileInputs.value.click();
+      });
+      const handleFileChange = ((event) => {
+        const files = event.target.files;
+        const imageExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+        //檢查檔名
+        for (let i = 0; i < files.length; i++) {
+          const fileName = files[i].name;
+          const fileExtension = fileName.slice(((fileName.lastIndexOf('.') - 1) >>> 0) + 2); //得到副檔名
+          if (!imageExtensions.includes(fileExtension.toLowerCase())) {
+            alert(fileExtension + '不在允許的格式範圍內，請重新選取');
+            return;
+          }
+        }
+        //圖片總數量不超過五張
+        if (formParams.newFile.length + files.length > 5) {
+          alert('上傳至多5張圖片');
+          return;
+        }
+        // 檢查圖片大小
+        if (!checkFileSize(files, formParams.newFile)) {
+          return
+        }
+        console.log(event.target.files);
+        // 压缩并处理图像
+        const imgArray = formParams.newFile;
+        const previewUrl = formParams.viewFile;
+        for (let i = 0; i < files.length; i++) {
+          const reader = new FileReader();
+          reader.onload = (e) => {
+            const file = files[i]; // 保持原始文件
+            imgArray.push(file);
+            previewUrl.push({
+              FileName: file.name,
+              FileLink: URL.createObjectURL(file),
+            });
+          };
+          reader.readAsDataURL(files[i]);
+        }
+        // console.log(formData[index].previewUrl);
+      });
+      const deleteFile = ((index) => {
+        formParams.newFile.splice(index, 1);
+        formParams.viewFile.splice(index, 1);
+      });
+      const viewImgFile = ((index) => {
+        modalParams.title = formParams.viewFile[index].FileName;
+        modalParams.src = formParams.viewFile[index].FileLink;
+      });
+      // 監聽formParams.AssetsId(資產編號)的數值變動 -> 搜尋
+      watch(() => formParams.AssetsId, (newValue, oldValue) => {
+        getAssets(newValue)
+          .then((data) => {
+            Assets.Name = data.AssetName;
+            Assets.Type = data.AssetType;
+            Assets.Status = data.Status;
+            // 檢查資產類型
+            if (Assets.Type === '耗材') {
+              wrongStatus.value = true;
+              canSubmit.value = false;
+              alertMsg.value = '僅提供資產類型為非耗材的物品進行維修'
+            } else {
+              // 檢查資產狀態(只有非耗材才會檢查)
+              const Status = Assets.Status
+              const Type = Assets.Type
+              wrongStatus.value = true;
+              canSubmit.value = false;
+              switch (Status) {
+                case '已被設備整合':
+                  alertMsg.value = `此${Type}已被設備整合，請先移出設備箱`
+                  break;
+                case '維修':
+                  alertMsg.value = `此${Type}已送修`
+                  break;
+                case '報廢':
+                  alertMsg.value = `此${Type}已${Status}`
+                  break;
+                case '出貨':
+                  alertMsg.value = `此${Type}已${Status}`
+                  break;
+                case '退貨':
+                  alertMsg.value = `此${Type}已${Status}`
+                  break;
+                default: // 可報修
+                  wrongStatus.value = false;
+                  canSubmit.value = true;
+                  alertMsg.value = ''
+                  break;
+              }
+            }
+          })
+          .catch((error) => {
+            wrongStatus.value = true;
+            canSubmit.value = false;
+            Assets.Name = '';
+            alertMsg.value = '請輸入正確的資產編號'
+          })
+      }, {
+        immediate: false
+      });
+      return {
+        Applicant,
+        ApplicationDate,
+        Assets,
+        formParams,
+        modalParams,
+        alertMsg,
+        wrongStatus,
+        canSubmit,
+        fileInputs,
+        // getAssetsInfo,
+        submit,
+        openFileExplorer,
+        handleFileChange,
+        deleteFile,
+        viewImgFile,
+        goBack,
+      }
+    },
+  };
+</script>
+
+
+<style lang="scss" scoped>
+  @import '@/assets/css/global.scss';
+  .button_wrap {
+    display: flex;
+    margin-top: 30px;
+    justify-content: center;
+    padding: 0 28%;
+    margin-bottom: 5%;
+    gap: 20px;
+    button.back_btn {
+      @include back_to_previous_btn;
+      &:hover {
+        background-color: #5d85bb;
+      }
+    }
+    .delete_btn {
+      background: var(--c-5, #E94B4B);
+      justify-content: center;
+      align-items: center;
+      display: inline-flex;
+      border-radius: 10px;
+      height: 40px;
+      width: 90px;
+      color: #FFF;
+      text-align: center;
+      font-size: 20px;
+      font-weight: 700;
+      border: none;
+      margin: 0 10px;
+      &:hover {
+        background-color: #a51e1e;
+      }
+    }
+  }
+  h1 {
+    text-align: center;
+    font-weight: 600;
+    @include title_color;
+  }
+  @media only screen and (min-width: 1200px) {
+    .main_section {
+      h1 {
+        margin-top: 100px;
+        font-size: 55px;
+      }
+    }
+  }
+  @media only screen and (min-width: 768px) and (max-width: 1199px) {
+    .main_section {
+      h1 {
+        margin-top: 100px;
+        font-size: 55px;
+      }
+    }
+  }
+  @media only screen and (max-width: 767px) {
+    .main_section {
+      h1 {
+        margin-top: 80px;
+        font-size: 40px;
+      }
+    }
+  }
+</style>
