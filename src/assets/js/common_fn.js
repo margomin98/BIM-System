6578e29168fa4dd6f8cf4450964e8b4b80ae32b2
nyxxxxx <s0941212@gmail.com>
2023-12-26 14:07:58 +0800

    [訂購] 新增測試

--- src/assets/js/common_fn.js
+++ src/assets/js/common_fn.js
@@ -12,6 +12,33 @@ export const getDate = (()=>{
 export const goBack = (()=> {
   window.history.back();
 });
+// 檢查必填
+export const checkRequire = ((checkList,formParams,fileParams)=>{
+  for(const key of checkList) {
+    if(!formParams[key]) {
+      alert('請輸入必填項目')
+      return false;
+    }
+  }
+  if(fileParams) {
+    if(fileParams.newDoc.length == 0) {
+      alert('請輸入必填項目')
+      return false;
+    }
+  }
+  return true;
+})
+export const checkMaxLetter = ((maxLengthList,formParams)=>{
+  for(const key in maxLengthList) {
+    const { field, max } = maxLengthList[key];
+    const regexPattern = new RegExp(`^[\\s\\S]{0,${max}}$`);
+    if(!regexPattern.test(formParams[key])) {
+      alert(`${field}不可輸入超過${max}字`);
+      return false;
+    }
+  }
+  return true;
+})
 // 檢查狀態是否可進入頁面(編輯、交付、刪除等需要鎖定狀態的頁面)
 export const canEnterPage = ((Status , Condition)=>{
   if(!Condition.includes(Status)) {
@@ -156,3 +183,95 @@ export const deleteFile = ((index,formParams,type) => {
 export const openFileExplorer = ((fileInputs) => {
   fileInputs.click();
 });
+// 文件上傳
+export const handleDocumentFile =((event,fileParams) => {
+  console.log('DocumentFiles:', event.target.files);
+  const files = event.target.files;
+  const acceptExtensions = ['jpg', 'jpeg', 'png', 'gif', 'pdf', 'doc', 'docx'];
+  const maxFileSize = 28 * 1024 * 1024; // 28MB
+  // 檢查副檔名 &檔案大小
+  for (let i = 0; i < files.length; i++) {
+    const fileName = files[i].name;
+    const fileExtension = fileName.slice(((fileName.lastIndexOf('.') - 1) >>> 0) + 2); //取得副檔名
+    if (!acceptExtensions.includes(fileExtension.toLowerCase())) {
+      alert(fileExtension + '不在允許的格式範圍內，請重新選取');
+      return;
+    }
+    if (files[i].size > maxFileSize) {
+      alert('檔案' + fileName + '大於28MB，請重新選取');
+      return;
+    }
+  }
+  // 處理檔案
+  const fileArray = fileParams.newDoc;
+  const previewUrl = fileParams.viewDoc;
+  for (let i = 0; i < files.length; i++) {
+    // 依據檔案格式 分為 1.圖片(可預覽) 2.pdf(可預覽) 3. doc/docx(可下載)
+    const file = files[i];
+    const fileName = file.name;
+    const fileExtension = fileName.slice(((fileName.lastIndexOf('.') - 1) >>> 0) + 2); //得到副檔名
+    const reader = new FileReader();
+    let type = '';
+    // .pdf/.doc/.docx
+    if (fileExtension === 'pdf' || fileExtension === 'doc' || fileExtension === 'docx') {
+      type = fileExtension;
+    }
+    // 圖片
+    else {
+      type = 'pic';
+    }
+    fileArray.push(file);
+    previewUrl.push({
+      FileName: file.name,
+      FileLink: URL.createObjectURL(file),
+      type: type,
+    });
+    reader.readAsDataURL(file);
+  }
+  // console.log('uploaded viewDoc:', fileParams.viewDoc);
+  // console.log('uploaded newDoc:', fileParams.newDoc);
+})
+export const handlePreview = ((file,modalParams) => {
+  // 先提取副檔名
+  // 以"."為基準分割字串
+  const part = file.FileName.split(".");
+  let extension = '';
+  // 如果part長度大於1表示xxxx.aaa => ['xxxx','aaa']
+  if (part.length > 1) {
+    extension = part[part.length - 1];
+  }
+  // 1. pdf 2. word 3. picture
+  switch (extension) {
+    case 'pdf':
+      window.open(file.FileLink)
+      break;
+    case 'doc':
+    case 'docx':
+      const downloadElement = document.getElementById('download-link');
+      downloadElement.href = file.FileLink;
+      downloadElement.download = file.FileName;
+      downloadElement.click();
+      break;
+    default:
+      modalParams.title = file.FileName;
+      modalParams.src = file.FileLink;
+      const modal = document.querySelector('#open_modal');
+      // console.log('modea',modal);
+      modal.click();
+      break;
+  }
+})
+export const deleteDocument = ((index, file ,fileParams) => {
+  // 1.
+  if (file.exist) {
+    fileParams.deleteDoc.push(file.FileName);
+    fileParams.existDocument.splice(index, 1);
+    console.log('已加入的deleteDoc:', fileParams.deleteDoc);
+  }
+  // 2.
+  else {
+    fileParams.newDoc.splice(index, 1);
+    fileParams.viewDoc.splice(index, 1);
+    console.log('剩餘newDoc:', fileParams.newDoc);
+  }
+})
--- src/views/order/New.vue
+++ src/views/order/New.vue
@@ -1,275 +1,153 @@
 <template>
-  <Navbar/>
+  <Navbar @username="getUserName"/>
   <div class="main_section">
     <div class="title col">
       <h1>
         新增訂購單
       </h1>
     </div>
-    <Order_component/>
+    <Order_component :hidden="hidden" :placeholder="placeholder"/>
     <div class="col button_wrap">
       <button class="back_btn" @click="goBack">回上一頁</button>
-      <button class="send_btn" :disabled="!canSubmit" :class="{send_btn_disabled: !canSubmit}" @click="submit">新增</button>
+      <button class="send_btn" :class="{'send_btn_disabled': loading}" @click="submit" :disabled="loading">新增</button>
     </div>
   </div>
 </template>
 
-<script>
-  import {
-    onMounted,
-    reactive,
-    ref,
-    watch
-  } from 'vue';
+<script setup>
+  import { provide, reactive, ref, } from 'vue';
   import Navbar from '@/components/Navbar.vue';
-  import Order_component from '@/components/Order_component.vue';
+  import Order_component from '@/components/order_page/Order_component.vue';
   import router from '@/router';
-  import {
-    getDate,
-    goBack,
-    checkFileSize,
-  } from '@/assets/js/common_fn.js'
-  import {
-    getApplication,
-    getAssets
-  } from '@/assets/js/common_api.js'
+  import { goBack, checkRequire, checkMaxLetter, } from '@/assets/js/common_fn.js'
   import axios from 'axios'
-  export default {
-    components: {
-      Navbar,
-      Order_component
+	const formParams = reactive({
+		PO_ID: '',
+		PurchaseNum: '',
+		Source: '',
+		Use: '',
+		PurchaseDate: '',
+		Executor: '',//承辦人員
+		Quantity: 1,
+	});
+  const fileParams = reactive({
+    newDoc: [],
+    viewDoc: [],
+    deleteDoc: [], //將刪除的 已上傳物流文件記錄於此
+    existDocument: [],
+  })
+  const hidden = {
+    div: {
+      warning: true,
+      existFile: true,
     },
-    setup() {
-      const Applicant = ref('');
-      const ApplicationDate = ref('');
-      const Assets = reactive({
-        Name: '',
-        Type: '',
-        Status: '',
-      });
-      const formParams = reactive({
-        AssetsId: '',
-        Question: '',
-        newFile: [],
-        viewFile: [], //不需要傳
-      });
-      const modalParams = reactive({
-        title: '',
-        src: '',
+    input: {
+      PO_ID: true,
+      // file_trashcan: true,
+    },
+  }
+  const placeholder = {
+    PurchaseNum: '最多50字',
+    Source: '最多50字',
+    Use: '最多100字',
+  }
+  const loading = ref(false);
+  provide("form",formParams);
+  provide("file",fileParams);
+  const getUserName = ((name)=>{
+    formParams.Executor = name;
+  })
+  const submit = (async ()=>{
+    const RequireCheckList = ['PurchaseNum','PurchaseDate','Quantity'];
+    const LetterCheckList = {
+      PurchaseNum: {field:'訂單編號',max:50},
+      Source: {field:'採購來源',max:50},
+      Use: {field:'使用專案',max:100},
+    }
+    // 檢查必填
+    if(!checkRequire(RequireCheckList,formParams,fileParams)) { return }
+    // 檢查字數
+    if(!checkMaxLetter(LetterCheckList,formParams)) { return }
+    loading.value = true;
+    try {
+      // 先用文字部分建立訂購單，再用訂購單ID(PO_ID)將檔案上傳
+      const PO_ID = await sendTextForm();
+      let filePromises = [];
+      fileParams.newDoc.forEach((file,index)=>{
+        filePromises.push(sendFileForm(PO_ID, file, index));
       })
-      const alertMsg = ref('');
-      const wrongStatus = ref(false);
-      const canSubmit = ref(false);
-      const fileInputs = ref(null);
-      onMounted(() => {
-        getApplicationInfo()
-        ApplicationDate.value = getDate()
-      });
-      async function getApplicationInfo() {
-        getApplication()
-          .then((data) => {
-            Applicant.value = data;
-          })
-          .catch((error) => {
-            console.error(error);
-          })
+      await Promise.all(filePromises)
+      .then((result)=>{
+        const allSuccess = result.every(result => result === 'success')
+        if (allSuccess) {
+          alert('新增訂購單成功\n單號為:' + PO_ID);
+          router.push({
+            name: 'Order_Datagrid'
+          });
+        } else {
+          alert('新增訂購單失敗');
+        }
+      })
+      .catch((e)=>{
+        console.error(e);
+      })
+    } 
+    catch (error) {
+      console.error(error);
+    }
+    finally {
+      loading.value = false;
+    }
+    // console.log('form',formParams);
+    // console.log('file',fileParams.newDoc);
+  })
+  const sendTextForm = (()=>{
+    return new Promise((resolve, reject) => {
+      const form = new FormData();
+      for (const key in formParams) {
+        if(formParams[key]) {
+          form.append(key, formParams[key]);
+        }
       }
-      // async function getAssetsInfo() {
-      //   getAssets(formParams.AssetsId)
-      //     .then((data)=>{
-      //       Assets.Name = data.AssetName;
-      //       Assets.Type = data.AssetType;
-      //       Assets.Status = data.Status;
-      //       // 檢查資產類型
-      //       if(Assets.Type === '耗材') {
-      //         wrongStatus.value = true;
-      //         canSubmit.value = false;
-      //         alertMsg.value = '僅提供資產類型為非耗材的物品進行維修'
-      //       }
-      //       // 檢查資產狀態(只有非耗材才會有這個Status)
-      //       else if(Assets.Status === '已被設備整合') {
-      //         wrongStatus.value = true;
-      //         canSubmit.value = false;
-      //         alertMsg.value = '此資產已被設備整合，請先移出設備箱'
-      //       }
-      //       // 可報修
-      //       else {
-      //         wrongStatus.value = false;
-      //         canSubmit.value = true;
-      //         alertMsg.value = ''
-      //       }
-      //     })
-      //     .catch((error) =>{
-      //       wrongStatus.value = true;
-      //       canSubmit.value = false;
-      //       Assets.Name = '';
-      //       alertMsg.value = '請輸入正確的資產編號'
-      //     })
-      // }
-      async function submit() {
-        const pattern = /^(BF\d{8})$/;
-        // 檢查必填項目、格式        
-        if (!pattern.test(formParams.AssetsId)) {
-          alert('資產編號格式錯誤');
-          return
-        }
-        if (!/^[\s\S]{0,500}$/.test(formParams.Question)) {
-          alert('問題描述不可超過500字');
-          return
-        }
-        const form = new FormData();
-        for (const key in formParams) {
-          if (formParams[key]) {
-            form.append(key, formParams[key]);
+      axios.post('http://192.168.0.177:7008/PurchasingMng/CreateOrder', form)
+        .then(response => {
+          const data = response.data;
+          if (data.state === 'success') {
+            const PO_ID = response.data.resultList.PO_ID;
+            console.log('新增文字部分表單成功PO_ID:', PO_ID);
+            resolve(PO_ID);
+          } else {
+            reject(data.messages);
           }
-        }
-        // 移除viewFile
-        form.delete('viewFile');
-        // newFile額外append
-        form.delete('newFile');
-        for (let i = 0; i < formParams.newFile.length; i++) {
-          form.append('newFile', formParams.newFile[i]);
-        }
-        axios.post('http://192.168.0.177:7008/RepairMng/CreateOrder', form)
-          .then((response) => {
-            const data = response.data;
-            if (data.state === 'success') {
-              alert('新增報修單成功\n單號為:' + data.resultList.R_ID);
-              router.push({
-                name: 'Repair_Datagrid'
-              });
-            } else if (data.state === 'account_error') {
-              alert(data.messages);
-              router.push('/');
-            } else {
-              alert('新增報修單失敗')
-            }
-          })
-          .catch((error) => {
-            console.error(error);
-          })
-      }
-      const openFileExplorer = (() => {
-        fileInputs.value.click();
-      });
-      const handleFileChange = ((event) => {
-        const files = event.target.files;
-        const imageExtensions = ['jpg', 'jpeg', 'png', 'gif'];
-        //檢查檔名
-        for (let i = 0; i < files.length; i++) {
-          const fileName = files[i].name;
-          const fileExtension = fileName.slice(((fileName.lastIndexOf('.') - 1) >>> 0) + 2); //得到副檔名
-          if (!imageExtensions.includes(fileExtension.toLowerCase())) {
-            alert(fileExtension + '不在允許的格式範圍內，請重新選取');
-            return;
-          }
-        }
-        //圖片總數量不超過五張
-        if (formParams.newFile.length + files.length > 5) {
-          alert('上傳至多5張圖片');
-          return;
-        }
-        // 檢查圖片大小
-        if (!checkFileSize(files, formParams.newFile)) {
-          return
-        }
-        console.log(event.target.files);
-        // 压缩并处理图像
-        const imgArray = formParams.newFile;
-        const previewUrl = formParams.viewFile;
-        for (let i = 0; i < files.length; i++) {
-          const reader = new FileReader();
-          reader.onload = (e) => {
-            const file = files[i]; // 保持原始文件
-            imgArray.push(file);
-            previewUrl.push({
-              FileName: file.name,
-              FileLink: URL.createObjectURL(file),
-            });
-          };
-          reader.readAsDataURL(files[i]);
-        }
-        // console.log(formData[index].previewUrl);
-      });
-      const deleteFile = ((index) => {
-        formParams.newFile.splice(index, 1);
-        formParams.viewFile.splice(index, 1);
-      });
-      const viewImgFile = ((index) => {
-        modalParams.title = formParams.viewFile[index].FileName;
-        modalParams.src = formParams.viewFile[index].FileLink;
-      });
-      // 監聽formParams.AssetsId(資產編號)的數值變動 -> 搜尋
-      watch(() => formParams.AssetsId, (newValue, oldValue) => {
-        getAssets(newValue)
-          .then((data) => {
-            Assets.Name = data.AssetName;
-            Assets.Type = data.AssetType;
-            Assets.Status = data.Status;
-            // 檢查資產類型
-            if (Assets.Type === '耗材') {
-              wrongStatus.value = true;
-              canSubmit.value = false;
-              alertMsg.value = '僅提供資產類型為非耗材的物品進行維修'
-            } else {
-              // 檢查資產狀態(只有非耗材才會檢查)
-              const Status = Assets.Status
-              const Type = Assets.Type
-              wrongStatus.value = true;
-              canSubmit.value = false;
-              switch (Status) {
-                case '已被設備整合':
-                  alertMsg.value = `此${Type}已被設備整合，請先移出設備箱`
-                  break;
-                case '維修':
-                  alertMsg.value = `此${Type}已送修`
-                  break;
-                case '報廢':
-                  alertMsg.value = `此${Type}已${Status}`
-                  break;
-                case '出貨':
-                  alertMsg.value = `此${Type}已${Status}`
-                  break;
-                case '退貨':
-                  alertMsg.value = `此${Type}已${Status}`
-                  break;
-                default: // 可報修
-                  wrongStatus.value = false;
-                  canSubmit.value = true;
-                  alertMsg.value = ''
-                  break;
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  })
+  const sendFileForm = (( PO_ID , file , index)=>{
+    return new Promise((resolve, reject) => {
+          const form = new FormData();
+          form.append('PO_ID', PO_ID);
+          form.append('num', index);
+          form.append('Document', file);
+          const axios = require('axios');
+          axios.post('http://192.168.0.177:7008/PurchasingMng/UploadFile', form)
+            .then((response) => {
+              const data = response.data;
+              if (data.state === 'success') {
+                console.log(`第${index+1}個文件上傳成功`);
+                resolve(data.state)
+              } else {
+                console.error(type + '上傳失敗，' + response.data.messages);
+                resolve('fail');
               }
-            }
-          })
-          .catch((error) => {
-            wrongStatus.value = true;
-            canSubmit.value = false;
-            Assets.Name = '';
-            alertMsg.value = '請輸入正確的資產編號'
-          })
-      }, {
-        immediate: false
-      });
-      return {
-        Applicant,
-        ApplicationDate,
-        Assets,
-        formParams,
-        modalParams,
-        alertMsg,
-        wrongStatus,
-        canSubmit,
-        fileInputs,
-        // getAssetsInfo,
-        submit,
-        openFileExplorer,
-        handleFileChange,
-        deleteFile,
-        viewImgFile,
-        goBack,
-      }
-    },
-  };
+            })
+            .catch(error => {
+              reject(error);
+            });
+        });
+  })
 </script>
 
 
